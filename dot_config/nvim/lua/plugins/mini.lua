67e9796cfbb493831b486a33246d27a90c992dc1
Ssnibles <joshua.breite@gmail.com>
2025-03-21 18:32:19 +1300

    update

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,19 +1,19 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		-- event = { "BufReadPost", "BufNewFile" },
-		config = function()
-			-- Basic setup for some commonly used mini modules
-			require("mini.ai").setup()
-			require("mini.align").setup()
-			require("mini.bufremove").setup()
-			require("mini.comment").setup()
-			require("mini.cursorword").setup()
-			require("mini.pairs").setup()
-			require("mini.statusline").setup()
-			require("mini.surround").setup()
-			require("mini.sessions").setup()
-		end,
-	},
+  {
+    "echasnovski/mini.nvim",
+    version = false,
+    -- event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      -- Basic setup for some commonly used mini modules
+      require("mini.ai").setup()
+      require("mini.align").setup()
+      require("mini.bufremove").setup()
+      require("mini.comment").setup()
+      require("mini.cursorword").setup()
+      require("mini.pairs").setup()
+      -- require("mini.statusline").setup()
+      require("mini.surround").setup()
+      require("mini.sessions").setup()
+    end,
+  },
 }
