279465a31585bfb3f0d3a8df36cd14cae243d284
Ssnibles <joshua.breite@gmail.com>
2025-05-30 23:31:40 +1200

    💄 Add waybar brightness indicator

--- dot_config/fish/aliases/scripts_aliases.fish
+++ dot_config/fish/aliases/scripts_aliases.fish
@@ -1,5 +1,5 @@
 # Usefull scripts
-alias re-add="~/Documents/scripts/chezmoi_add.sh"
+alias re-add="~/scripts/chezmoi/chezmoi_add.sh"
 alias re-shell="source ~/.config/fish/config.fish"
 alias line-count="~/Documents/scripts/line_count.sh"
 alias hypr-session="~/.config/hypr/scripts/hypr-session"
