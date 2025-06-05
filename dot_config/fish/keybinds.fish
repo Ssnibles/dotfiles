b8f8ea07455762cf19cba3207607c4022daba4eb
Ssnibles <joshua.breite@gmail.com>
2025-06-05 19:28:12 +1200

    🎨 Some small structure tweaks

--- dot_config/fish/keybinds.fish
+++ dot_config/fish/keybinds.fish
@@ -1,3 +1,8 @@
 # bind \cl 'clear; commandline -f repaint'
 # bind \cx 'echo "Hello, Fish!"'
 
+# Unbind keys
+function fish_user_key_bindings
+    bind --erase --preset \ev
+    # Add more unbinds as needed
+end
