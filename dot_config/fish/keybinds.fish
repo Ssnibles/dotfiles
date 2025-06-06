d14cf0f9a80cf5992829f7b461140f2bc6090e28
Ssnibles <joshua.breite@gmail.com>
2025-06-06 19:35:43 +1200

    🔥 Remove dead code

--- dot_config/fish/keybinds.fish
+++ dot_config/fish/keybinds.fish
@@ -1,6 +1,9 @@
 # bind \cl 'clear; commandline -f repaint'
 # bind \cx 'echo "Hello, Fish!"'
 
+bind \ct tmux
+bind \cn nvim
+
 # Unbind keys
 function fish_user_key_bindings
     bind --erase --preset \ev
